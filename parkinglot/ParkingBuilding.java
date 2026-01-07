5a14a73a26f6abf2a2b8b8861184183eef42c855
ShalinShikhar <Shalin@5207>
2026-01-08 00:45:07 +0530

    Day-3 Levels and building started.

--- parkinglot/ParkingBuilding.java
+++ parkinglot/ParkingBuilding.java
@@ -1,4 +1,15 @@
 package parkinglot;
 
+import pricing.CostComputation;
+
+import java.util.List;
+
 public class ParkingBuilding {
+
+    List<ParkingLevel> levels;
+    public ParkingBuilding(List<ParkingLevel> levels, CostComputation costComputation)
+    {
+        this.levels=levels;
+    }
+
 }
