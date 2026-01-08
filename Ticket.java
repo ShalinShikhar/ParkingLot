bc602f1f335708ace85a0ac0990df885baf89e98
ShalinShikhar <Shalin@5207>
2026-01-09 00:55:35 +0530

    Day-4

--- Ticket.java
+++ Ticket.java
@@ -1,2 +1,38 @@
+import Entity.ParkingSpot;
+import Entity.Vehicle;
+import parkinglot.ParkingLevel;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Ticket {
+
+    private final Vehicle vehicle;
+    private final ParkingLevel level;
+    private final ParkingSpot spot;
+    private final LocalDateTime entryTime;
+
+    public Ticket(Vehicle vehicle,ParkingLevel level,ParkingSpot spot)
+    {
+        this.vehicle=vehicle;
+        this.level=level;
+        this.spot=spot;
+        this.entryTime=LocalDateTime.now();
+    }
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public ParkingLevel getLevel() {
+        return level;
+    }
+
+    public ParkingSpot getSpot() {
+        return spot;
+    }
+
+    public LocalDateTime getEntryTime() {
+        return entryTime;
+    }
+
 }
--- parkinglot/ParkingBuilding.java
+++ parkinglot/ParkingBuilding.java
@@ -1,5 +1,7 @@
 package parkinglot;
 
+import Entity.ParkingSpot;
+import Entity.Vehicle;
 import pricing.CostComputation;
 
 import java.util.List;
@@ -10,6 +12,28 @@ public class ParkingBuilding {
     public ParkingBuilding(List<ParkingLevel> levels, CostComputation costComputation)
     {
         this.levels=levels;
+    }
+
+    Ticket allocalte(Vehicle vehicle)
+    {
+        for(ParkingLevel parkingLevel : levels)
+        {
+            if(parkingLevel.hasAvaibility(vehicle.getVehicleType()))
+            {
+                ParkingSpot spot=parkingLevel.park(vehicle.getVehicleType());
+                if(spot!=null)
+                {
+                    Ticket ticket=new Ticket(vehicle,parkingLevel,spot);
+                    System.out.println("Parking allocated at level : "+parkingLevel.getLevelNumber()+"spot : "+spot.getSpotId());
+                    return ticket;
+                }
+            }
+        }
+        throw new RuntimeException("parking full");
+    }
+    void release(Ticket ticket)
+    {
+        ticket.getlevel().unPark(ticket.getVehicle().getVehicleType(),ticket.getSpot());
     }
 
 }
